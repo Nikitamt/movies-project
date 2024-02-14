762cd7d935adbb7a997010238fbe9e84a13e457c
Nikitamt <ntarasov0326@icloud.com>
2024-02-14 10:05:15 +0300

    The project has been cleaned up, the header and footer have been made

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return <footer class="page-foote green lighten-3">
+    <div class="footer-copyright">
+      <div class="container">
+        © {new Date().getFullYear()} Copyright Text
+        <a class="grey-text text-lighten-4 right" href="#!">Repo</a>
+      </div>
+    </div>
+  </footer>
+}
+
+export { Footer } 
